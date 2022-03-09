52b7e557dc0b15ec75f51c0210b22dcc8ced0367
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-08 21:16:10 -0600

    button onclick js

---
+++ js/kittycards.js
@@ -0,0 +1,30 @@
+function brooklynButton() {
+
+  let brooklyn = '<div class="card">';
+  brooklyn += '<h2>' + 'Brooklyn' + '</h2>';
+  brooklyn += '<img src="img/Brooklyn.JPG" alt="Brooklyn"/>';
+  brooklyn += '<p>' + 'Brooklyn thinks she is a person, and she is Mamas favorite cat.' + '</p>';
+  brooklyn += '</div>';
+  document.getElementById("brooklyn").innerHTML = brooklyn;
+
+}
+
+function bunnyButton() {
+
+    let bunny = '<div class="card">';
+    bunny +='<h2>' + 'Bunny' + '</h2>';
+    bunny +='<img src="img/Bunny.jpg" alt="Bunny"/>';
+    bunny +='<p>' + 'Brooklyn thinks she is a person, and she is Mamas favorite cat.' + '</p>';
+    bunny +='</div>';
+    document.getElementById("bunny").innerHTML = bunny;
+  }
+
+  function abbyButton() {
+
+    let abby = '<div class="card">';
+    abby +='<h2>' + 'Abby' + '</h2>';
+    abby +='<img src="img/Abby.JPG" alt="Abby"/>';
+    abby +='<p>' + 'Abby is ferocious, and she is Papas favorite cat.' + '</p>';
+    abby +='</div>';
+    document.getElementById("abby").innerHTML = abby;
+  }
