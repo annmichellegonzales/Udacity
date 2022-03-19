9d96176d9e8a3e2f5601dfd2fd80990395df5e42
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-19 16:27:45 -0500

    feat: story intro

--- adventure_game.py
+++ adventure_game.py
@@ -1,4 +1,10 @@
-intro = ("Welcome, warrior. We do not have time to spare. Princess Pinky is in danger, and it is up to you to rescue her. " "Please hurry! Save the princess!")
-weapon = ("I can only offer you a weapon")
+import time
 
-print(intro, weapon)
+print("Welcome, warrior.\n" "We do not have time to spare!")
+time.sleep(2)
+print("Princess Pinky is in danger, and it is up to you to rescue her.")
+time.sleep(2)
+print("Please hurry!")
+time.sleep(1)
+print("I can only offer you a weapon: \n")
+print("Jambi presents to you a sword and a mace. Please enter 1 to choose the sword, or enter 2 to choose the mace")
