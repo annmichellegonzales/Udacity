58a29bba04f1daa7e5aa572fe4d6b20b977d4c32
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-19 15:20:23 -0500

    fix: elif

---
+++ breakfast_bot.py
@@ -0,0 +1,6 @@
+response = input("Would you like pancakes, or waffles?\n")
+
+if (response == "pancakes"):
+    print("Great! Pancakes it is")
+elif (response == "waffles"):
+    print("Cool! Waffles comin' right up!")
