cf788dfb5fee38b649198bd7e2881593ddcda5fe
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-21 11:44:03 -0500

    refactor: def print_pause

--- adventure_game.py
+++ adventure_game.py
@@ -1,27 +1,24 @@
 import time
 
-# create function, example of print_pause
-# def print_pause():
-#     time.sleep(2) + print
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(2)
 
-print("Welcome, warrior.\n" "We do not have time to spare!")
-time.sleep(2)
-print("Princess Pinky is in danger, and it is up to you to rescue her.")
-time.sleep(2)
-print("Please hurry!")
-time.sleep(1)
-print("I can only offer you a weapon: \n")
-print("Jambi presents two weapons for you to choose.")
+print_pause("Welcome, warrior.\n" "We do not have time to spare!")
+print_pause("Princess Pinky is in danger, and it is up to you to rescue her.")
+print_pause("Please hurry!")
+print_pause("I can only offer you a weapon: \n")
+print_pause("Jambi presents two weapons for you to choose. A shiny sword, or a plain looking mace")
 
 while True:
     weapon = input("Please enter 1 to choose the sword, or enter 2 to choose the mace.\n")
     if weapon == "1":
-        print("Excellent choice! This sword is imbued with the mystical properties of Lake Envalore! \n"
+        print_pause("Excellent choice! This sword is imbued with the mystical properties of Lake Envalore! \n"
         "You receive your weapon, and follow Jambi's directions to rescue Princes Pinky.")
         break
     elif weapon == "2":
-        print("Although this is a fierce mace, it is not enchanted. \n"
+        print_pause("Although this is a fierce mace, it is not enchanted. \n"
         "You receive your weapon, and follow Jambi's directions to rescue Princes Pinky.")
         break
     else:
-        print("Enter 1 or 2 to continue.")
+        print_pause("Enter 1 or 2 to continue.")
