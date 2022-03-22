8394fd3a5253da30b87ed86080310ebf43ef5146
annmichellegonzales <ann.michelle.gonzales112@gmail.com>
2022-03-22 16:32:46 -0500

    feat: play again

--- adventure_game.py
+++ adventure_game.py
@@ -4,21 +4,44 @@ def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
 
-print_pause("Welcome, warrior.\n" "We do not have time to spare!")
-print_pause("Princess Pinky is in danger, and it is up to you to rescue her.")
-print_pause("Please hurry!")
-print_pause("I can only offer you a weapon: \n")
-print_pause("Jambi presents two weapons for you to choose. A shiny sword, or a plain looking mace")
+def valid_input(prompt, option1, option2):
+    while True:
+        response = input(prompt).lower()
+        if option1 in response:
+            break
+        elif option2 in response:
+            break
+        else:
+            print_pause("Sorry, I don't understand.")
+    return response
 
-while True:
-    weapon = input("Please enter 1 to choose the sword, or enter 2 to choose the mace.\n")
-    if weapon == "1":
-        print_pause("Excellent choice! This sword is imbued with the mystical properties of Lake Envalore! \n"
-        "You receive your weapon, and follow Jambi's directions to rescue Princes Pinky.")
-        break
-    elif weapon == "2":
-        print_pause("Although this is a fierce mace, it is not enchanted. \n"
-        "You receive your weapon, and follow Jambi's directions to rescue Princes Pinky.")
-        break
+def intro():
+    print_pause("Welcome, warrior. We do not have time to spare!")
+    print_pause("Princess Pinky is in danger, and it is up to you to rescue her.")
+    print_pause("I can only offer you a weapon: ")
+    print_pause("Jambi presents two weapons for you to choose. A shiny sword, or a plain looking mace.")
+
+def choose_weapon():
+    choice = input("Please enter 1 to choose the sword, or enter 2 to choose the mace.\n")
+    if choice == 1:
+        print_pause("Excellent choice! This sword is imbued with the mystical properties of Lake Envalore!")
+    elif choice == 2:
+        print_pause("Although this is a fierce mace, it is not enchanted.")
     else:
-        print_pause("Enter 1 or 2 to continue.")
+        print_pause("Please save her!")
+        return choice
+
+
+intro()
+while True:
+
+    choose_weapon()
+    print_pause("You receive your weapon, and follow Jambi's directions to rescue Princes Pinky.")
+    play_again = valid_input("Would you like to play again? "
+                              "Please say 'yes' or 'no'.\n",
+                              "yes", "no")
+    if "no" in play_again:
+        print_pause("Thanks for playing! Good bye.")
+        break
+    elif "yes" in play_again:
+        print_pause("Our princess does seem to get herself into trouble more often than not. Please rescue her again!")
